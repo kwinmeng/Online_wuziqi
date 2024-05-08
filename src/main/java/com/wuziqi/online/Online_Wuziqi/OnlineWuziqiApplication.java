5259e0eda7e237fe24a02b167f46a7db21526f76
小钟 <211220086@smail.nju.edu.cn>
2024-05-08 22:56:58 +0800

    init

---
+++ src/main/java/com/wuziqi/online/Online_Wuziqi/OnlineWuziqiApplication.java
@@ -0,0 +1,13 @@
+package com.wuziqi.online.Online_Wuziqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineWuziqiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineWuziqiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuziqi/online/Online_Wuziqi/OnlineWuziqiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuziqi.online.Online_Wuziqi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineWuziqiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
